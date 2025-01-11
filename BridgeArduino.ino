312959f54347ddbf590cda23a950835e99243c03
Kotokott <kraynovgr@gmail.com>
2025-01-11 21:45:04 +0300

    Added arduino bridge file

---
+++ BridgeArduino.ino
@@ -0,0 +1,28 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Output(); //да, я очень люблю создавать функции для каждого действия:^
+}
+
+void Output() {
+  Serial.println("12070"); //отправка валидного uid
+  Input();
+  delay(1000);
+  Serial.println("161879"); // отправка невалидного uid
+  Input();
+  delay(1000);
+}
+
+void Input() {
+  if (Serial.available() > 0) {  //если сериал порт доступен
+  String data = Serial.readStringUntil('\n'); //принятие данных до новой строки
+  if (data == "allow") {
+  //если получено разрешение
+  }
+  if (data == "forbid") {
+  //если получен отказ
+  }
+  }
+}
